f2a4b45d2c4ed1b40e5ce48ac05f489ec72cb868
Sekinat <bashirsekinat10@gmail.com>
2022-10-13 07:03:09 -0700

    Unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+
+fprintf(stderr, "%s", "and that piece of art is useful\"");
+
+fprintf(stderr, "%s", " - Dora Korpar, 2015-10-19\n");
+
+return (1);
+
+}
