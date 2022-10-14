6298c8d8c5bc9ff7c8e7c7124667fe6f69787872
Sekinat <bashirsekinat10@gmail.com>
2022-10-14 15:43:28 -0700

    Print numbers 0 to 9

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Description: prints all single digit numbers of base 10 starting from 0 to 9
- * Result: Always 0 (Success)
+ *main - Entry point
+ *Description: Print numbers 0 to 9
+ *Result: Always 0 (Success)
  */
-
 int main(void)
-
 {
+	int num;
 
-   int num;
-
-   for (num = 0; num <= 9; num++)
-             printf("%d", num);
-   printf("\n");
-
- return (0);
-
+	for (num = 0; num <= 9; num++)
+		printf("%d", num);
+	printf("\n");
+		return (0);
 }
