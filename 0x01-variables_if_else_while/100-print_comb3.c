bc60bae361d2e25d681f121484d7446c8e6c93eb
Sekinat <bashirsekinat10@gmail.com>
2022-10-14 17:47:15 -0700

    Prints all combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,14 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 < 10; num1++)
+	for (num1 = 48; num1 <= 56; num1++)
 	{
-		for (num2 = 0; num2 < 10; num2++)
+		for (num2 = num1 + 1; num2 <= 57; num2++)
 		{
-			putchar((num1 % 10) + '0');
-			putchar((num2 % 10) + '0');
+			putchar((char)num2);
+			putchar((char)num2);
 
-			if (num1 == 9 && num2 == 9)
+			if (num1 != 56)
 
 				continue;
 
