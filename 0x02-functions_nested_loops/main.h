12d3638b32a90e17398d8e52bbe5c577e4115f08
Sekinat <bashirsekinat10@gmail.com>
2022-10-18 15:13:16 -0700

    Putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,18 @@
-#include <stdio.h>
 
-/**
- * main
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-	return (0);
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+void print_to_98(int n);
+int add(int, int);
+void times_table(void);
+void jack_bauer(void);
+int print_last_digit(int n);
+int _abs(int);
+int print_sign(int n);
 
-}
+#endif
