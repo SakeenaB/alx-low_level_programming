9ef9b277e0c5892934e59bc639be3b05da14e7fb
Sekinat <bashirsekinat10@gmail.com>
2022-10-18 17:13:35 -0700

    adds two integers and returns the result

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,10 +7,10 @@ void times_table(void)
 {
 	int i, j, product;
 
-	for (i = 0; i <= 9; ++i)
+	for (i = 0; i <= 9; i++)
+	{
 
-		{
-			for (j = 0; j <= 9; ++j)
+			for (j = 0; j <= 9; j++)
 			{
 
 				product = i * j;
