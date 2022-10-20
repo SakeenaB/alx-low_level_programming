dec38b9de0521a2a0cd20a9d594d418539847eee
Sekinat <bashirsekinat10@gmail.com>
2022-10-20 07:17:08 -0700

    Prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,38 +8,16 @@
 
 void print_number(int n)
 {
-	long m;
-	int c;
-	long a;
+	unsigned int i = n;
 
-	a = n;
-
-	if (a < 0)
+	if (n < 0)
 	{
-		a *= -1;
 		_putchar('-');
+		i = -i;
 	}
-	m = 1;
-	c = 1;
 
-	while (c)
-	{
-		if (a / (m * 10) > 0)
-			m *= 10;
-		else
-			c = 0;
-	}
-	while (a >= 0)
-	{
-		if (m == 1)
-		{
-			_putchar(a % 10 + '0');
-			a = -1;
-		}
-		else
-		{
-			_putchar((a / m % 10) + '0');
-			m /= 10;
-		}
-	}
+	if ((i / 10) > 0)
+		print_number(i / 10);
+
+	_putchar((i % 10) + '0');
 }
