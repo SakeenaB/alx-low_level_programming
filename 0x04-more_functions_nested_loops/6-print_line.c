9670b24327d94cfc72737ba7bafdb729d86800b6
Sekinat <bashirsekinat10@gmail.com>
2022-10-20 04:33:17 -0700

    Draws a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line
+ * @n: number of lines
+ * Return: Always 0 (Success)
+ */
+
+void print_line(int n)
+
+{
+	int a;
+
+		if (a <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			for (a = 1; a <= n; a++)
+				_putchar('_');
+		}
+		_putchar('\n');
+}
