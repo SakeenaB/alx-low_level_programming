ac5954c992a20668dca5472bd59d48be11250823
Sekinat <bashirsekinat10@gmail.com>
2022-10-24 15:53:13 -0700

    a function that returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be counted
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+
+	return (l);
+}
